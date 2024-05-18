ec0559f69d15ca7784076b1c43f6db7221753e21
zecola <xyk816@gmail.com>
2024-05-18 15:06:36 +0800

    UserInterfaceInfo 初始化

--- src/main/java/com/zecola/project/model/dto/userInterfaceInfo/UserInterfaceInfoUpdateRequest.java
+++ src/main/java/com/zecola/project/model/dto/userInterfaceInfo/UserInterfaceInfoUpdateRequest.java
@@ -18,16 +18,6 @@ public class UserInterfaceInfoUpdateRequest implements Serializable {
     private Long id;
 
     /**
-     * 调用用户 id
-     */
-    private Long userId;
-
-    /**
-     * 接口 id
-     */
-    private Long interfaceInfoId;
-
-    /**
      * 总调用次数
      */
     private Integer totalNum;
@@ -41,4 +31,6 @@ public class UserInterfaceInfoUpdateRequest implements Serializable {
      * 0-正常，1-禁用
      */
     private Integer status;
+
+    private static final long serialVersionUID = 1L;
 }
